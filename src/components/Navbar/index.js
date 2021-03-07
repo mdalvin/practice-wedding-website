88453a80291e78b205ea2d86aadde2145afae431
mdalvin <mochamaddalvin@gmail.com>
2021-03-07 21:55:18 +0700

    first commit and Navbar added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavItem,
+  NavLink,
+  NavLogo,
+  NavMenu,
+} from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo>The Wedding</NavLogo>
+        <MobileIcon>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          <NavItem>
+            <NavLink
+              to="about"
+              smooth={true}
+              duration={500}
+              exact="true"
+              offset={-80}
+            >
+              About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              to="venue"
+              smooth={true}
+              duration={500}
+              exact="true"
+              offset={-80}
+            >
+              Venue
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              to="map"
+              smooth={true}
+              duration={500}
+              exact="true"
+              offset={-80}
+            >
+              Map
+            </NavLink>
+          </NavItem>
+        </NavMenu>
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
